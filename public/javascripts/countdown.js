c7de9446cf79feb64e205ccdb07c8c13367888be
HdH85 <haakondh@hotmail.com>
2025-07-30 12:41:45 +0200

    Countdown revision.

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -1,5 +1,30 @@
-﻿let timer = await db.question.time;
+﻿const db = require('../models');
+const QuestionService = require('../services/QuestionService.js')
+const questionService = new QuestionService(db);
 
-const startTimer = setInterval(function() {
-    timer--;
-});
+async function startTheCount(id) {
+    try {
+        const question = await questionService.getQuestionById(id);
+        let timer = parseInt(question.time);
+
+        const startTimer = setInterval(function() {
+            timer = timer - 1;
+            const milliseconds = Math.floor(total % 1000);
+            const seconds = Math.floor(total % 60);
+            const minutes = Math.floor(total / 60);
+            
+            if (timer <= 0) {
+                clearInterval(startTimer);
+            }
+
+            return {
+                minutes,
+                seconds,
+                milliseconds
+            };
+        }, 1000);
+        
+    } catch (error) {
+        console.log('Countdown error', error);
+    }
+}
